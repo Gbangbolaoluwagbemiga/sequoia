2f85a16161416db543ef29f83acaa92a5f8d9b5e
oluwagbemiga <oluwagbemigagbangbola@gmail.com>
2026-01-05 17:56:15 +0100

    build: add hardhat and its toolbox as dev dependencies

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,7 +7,7 @@ module.exports = {
   networks: {
     baseSepolia: {
       url: "https://sepolia.base.org",
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : (process.env['PRIVATE-KEY'] ? [process.env['PRIVATE-KEY']] : []),
     },
   },
 };
