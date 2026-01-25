84c5eeb80577386d8dd27af5c5921fa28f3d5b9a
oluwagbemiga <oluwagbemigagbangbola@gmail.com>
2026-01-25 23:18:50 +0100

    Feat: Add contract attachment to interact script

--- scripts/interact.js
+++ scripts/interact.js
@@ -1,7 +1,15 @@
 const hre = require("hardhat");
 
 async function main() {
-  console.log("Interacting with Payeer...");
+  const contractAddress = process.env.CONTRACT_ADDRESS;
+  if (!contractAddress) {
+    console.error("Please set CONTRACT_ADDRESS in .env");
+    return;
+  }
+  
+  const Payeer = await hre.ethers.getContractFactory("Payeer");
+  const payeer = await Payeer.attach(contractAddress);
+  console.log("Attached to Payeer at:", contractAddress);
 }
 
 main().catch((error) => {
